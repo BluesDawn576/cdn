7ffc582cd8e587838240a62a22fe37530bb43b21
unknown <1355502473@qq.com>
2020-07-24 19:00:27 +0800

    更新内容

---
+++ dawnline/user/js/chart.js
@@ -0,0 +1,88 @@
+var arr1=[],arr2=[],arr3=[];
+var  myChart = echarts.init(document.getElementById('chart'));
+function arrTest(){
+            $.ajax({
+              type:"get",
+              async:false,
+              url:"level.php",
+              data:{},
+              dataType:"json",
+              success:function(result){
+                if (result) {
+                    for (var i = 0; i < result.length; i++) {
+                      arr1.push(result[i].Name);
+                      arr2.push(result[i].pass);
+                      arr3.push(result[i].perfect); 
+                  }
+                }
+              }
+            })
+            return arr1,arr2,arr3;
+          }
+          arrTest();
+option = {
+tooltip: {
+    show: true,
+    /*trigger: 'axis',
+    axisPointer: {
+        type: 'cross',
+        label: {
+            backgroundColor: '#6a7985'
+        }
+    }*/
+},
+legend: {
+    data: ['Pass','Perfect'],
+    textStyle: {
+        color: '#4F94CD'
+    }
+},
+xAxis: {
+    type: 'category',
+    //boundaryGap: false,
+    data: arr1,
+    axisLine: {
+        lineStyle: {
+            color: '#4F94CD'
+        }
+    }
+},
+yAxis: {
+    type: 'value',
+    axisLine: {
+        lineStyle: {
+            color: '#4F94CD'
+        }
+    }
+},
+series: [
+    {
+    name: 'Pass',
+    data: arr2,
+    type: 'bar',
+    areaStyle: {},
+    markPoint : {
+        data:[
+        {type : 'max', name: 'max'},
+        {type : 'min', name: 'min'}
+        ]
+    }
+},
+{
+    name: 'Perfect',
+    data: arr3,
+    type: 'bar',
+    areaStyle: {},
+    markPoint : {
+        data:[
+        {type : 'max', name: 'max'},
+        {type : 'min', name: 'min'}
+        ]
+    }
+},
+],
+};
+
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
